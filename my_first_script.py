004df20aeb9bc27ae0f67db606fa38789afadc28
Larry Cousino <lawrence.cousino@gmail.com>
2024-12-12 19:14:16 -0500

    Make these changes

--- my_first_script.py
+++ my_first_script.py
@@ -1,1 +1,26 @@
-print("Hello, World!")
+# print("Hello, World!")
+
+# Day 2 Exercise
+# =======================================================
+# Exercise 1
+bread = 3.25
+steak = 8.40
+milk = 2.51
+total_cost = bread + steak + milk
+
+print(f"Your total is: ${total_cost}")
+# =======================================================
+# Exercise 2
+
+# Obtain store name from user
+store_name = input("What's the name of your favorite grocery store?")
+
+# Welcome user to their favorite store
+print(f"Welcome to {store_name}!")
+# ========================================================
+# Exercise 3
+milk = int("3")
+bread = 2.5
+total = str(milk + bread)
+print("The total cost is: $" + total)
+
